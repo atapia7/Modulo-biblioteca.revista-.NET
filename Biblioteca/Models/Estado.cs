bd1b0b9c623eaf45af38376b4fe08206bfb8c8b5
AndreeMichael <michaeltapialmidon07@gmail.com>
2022-10-13 19:09:31 -0500

    Revert "Agregar archivos de proyecto."
    This reverts commit e12f5202a1f361fe5ad7560c2944974ce868080c.

--- Biblioteca/Models/Estado.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace Biblioteca.Models
-{
-    public class Estado
-    {
-        public int IdEstado { get; set; }
-        public string NombreEstado { get; set; }
-    }
-}
